f1d42ea3bd098c992d325576ed6c8724a8923305
562706-Alfie <562706@escg.ac.uk>
2026-04-24 12:14:38 +0100

    14

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Use this script to manage the timer, score, and player health of the game
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
