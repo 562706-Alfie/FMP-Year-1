086b22d0e8dabfcf6ca452efc485595454750fc1
562706-Alfie <562706@escg.ac.uk>
2026-05-01 17:00:48 +0100

    23

--- Assets/Scripts/Camera/Camera No Rotation.cs
+++ Assets/Scripts/Camera/Camera No Rotation.cs
@@ -6,6 +6,7 @@ public class camera_no_rotate : MonoBehaviour
     [SerializeField] float cameraHeight, cameraSize;
     public bool isCameraLocked = true;
     public PlayerScript PlayerScript;
+    //33, 45, 70
 
     void Start()
     {
@@ -14,17 +15,15 @@ public class camera_no_rotate : MonoBehaviour
     void Update()
     {
         // Locks camera height, increases and scales up when player moves up 
-        if (PlayerScript.ypos < 70)
+        if (PlayerScript.ypos < 36)
         {
             transform.position = new Vector3(transform.position.x, cameraHeight, transform.position.z);
+            Camera.main.orthographicSize = cameraSize;
+
         }
         else
         {
-            print("camera height increasing");
             Camera.main.orthographicSize = Camera.main.orthographicSize + PlayerScript.yvel * Time.deltaTime;
         }
-
-            print("camera y = " + transform.position.y);
     }
-
 }
